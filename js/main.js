fc4cbc054056ad37960e42e82d2187f4b3eeca40
Carol Anderia Maria <cm8510823@gmail.com>
2023-09-12 23:42:52 +0530

    files added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('active')
+    menu.classList.toggle('move');
+}
+
+// costom scroll bar
+window.onscroll = function() {myfunction()};
+
+function myfunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
+
+
+
+
